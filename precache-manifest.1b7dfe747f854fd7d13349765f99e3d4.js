0234f078dc113254a9670a0965d68c02cfe6e1a8
MiguelSarmiento <sarmientomiguel3@gmail.com>
2020-08-31 18:54:45 -0500

    Updates

---
+++ precache-manifest.1b7dfe747f854fd7d13349765f99e3d4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d08c874cfbd2e6c4c7f73d1ae4d37de",
+    "url": "/portafolio/portafolio01personal/index.html"
+  },
+  {
+    "revision": "d8feda0084b60161a016",
+    "url": "/portafolio/portafolio01personal/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6ea3e6ed74a760794a68",
+    "url": "/portafolio/portafolio01personal/static/css/main.04547362.chunk.css"
+  },
+  {
+    "revision": "d8feda0084b60161a016",
+    "url": "/portafolio/portafolio01personal/static/js/2.2e308657.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portafolio/portafolio01personal/static/js/2.2e308657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea3e6ed74a760794a68",
+    "url": "/portafolio/portafolio01personal/static/js/main.44216403.chunk.js"
+  },
+  {
+    "revision": "5afb58f8c4677dd5f63d",
+    "url": "/portafolio/portafolio01personal/static/js/runtime-main.fb56f183.js"
+  },
+  {
+    "revision": "556626e7f8ccc35399f931219f1ec75f",
+    "url": "/portafolio/portafolio01personal/static/media/dota-img-1.556626e7.jpg"
+  },
+  {
+    "revision": "1ebf41a0ca9a3c5d64fab0b26c5f0b80",
+    "url": "/portafolio/portafolio01personal/static/media/dota-img-2.1ebf41a0.jpg"
+  },
+  {
+    "revision": "51f5f3d022b4a2bcdd6d5b5cc3d14c74",
+    "url": "/portafolio/portafolio01personal/static/media/dota-img-3.51f5f3d0.jpg"
+  },
+  {
+    "revision": "e5fdeac7a5aabf4d5b5656e4473de753",
+    "url": "/portafolio/portafolio01personal/static/media/fondo.e5fdeac7.jpg"
+  },
+  {
+    "revision": "f792bb4cc79a3c5f212fe2be950b75be",
+    "url": "/portafolio/portafolio01personal/static/media/logo-dota.f792bb4c.jpg"
+  },
+  {
+    "revision": "02c3e4f2991d3c68b4dfbab369481156",
+    "url": "/portafolio/portafolio01personal/static/media/logo-rata.02c3e4f2.png"
+  }
+]);
